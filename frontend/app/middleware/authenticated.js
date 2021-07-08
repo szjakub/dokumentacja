d71e0d2165dffbaccac638091e8ebd5debb029c1
Patryk Szlachta <patryk.szla@interia.pl>
2021-07-08 18:44:04 +0200

    change authennticated

--- frontend/app/middleware/authenticated.js
+++ frontend/app/middleware/authenticated.js
@@ -1,7 +1,7 @@
 export default function ({ store, redirect }) {
   // If the user is not authenticated
   if (store.state.user.token == null) {
-    return redirect('/login')
+    // return redirect('/login')
   } else {
     store.$axios.setToken(store.state.user.token, 'Token')
   }
